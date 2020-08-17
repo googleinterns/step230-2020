f318b3dc1a44ec06cd448cd562ca7c65ac140cb0
mara <marapislar@google.com>
2020-08-17 07:27:51 +0000

    Minor changes

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -7,12 +7,39 @@
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
+ 
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// difact_idibuted under the License is difact_idibuted on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
  
-function analyseText() {
-  let message = document.getElementById("card-message");
-  console.log(message.value);
+function login() {
+    fetch('/login_page').then(response => response.json()).then((login) => {
+        const page = document.getElementById('log');
+        const loginElement = document.createElement('div');
+        loginElement.className = 'login';
+        loginElement.innerHTML = login.message;
+        page.appendChild(loginElement);
+        if (login.status) {
+            document.getElementById("content").style.visibility = "visible";
+            loginElement.style.marginLeft = "1300px";
+        } else {
+            document.getElementById("content").style.visibility = "hidden";
+            loginElement.style.marginLeft = "auto";
+            loginElement.style.marginTop = "200px";
+        }
+        const load = document.getElementById('login_page');
+        load.style.visibility = "hidden";
+    });
+}
+ 
+function loadPostcard() {
+  fetch('/text-input').then(response => response.json()).then((output) => {
+    const ratingElement = document.getElementById("output");
+    ratingElement.innerText = "Your mood: " + String(output);
+  });
 }
