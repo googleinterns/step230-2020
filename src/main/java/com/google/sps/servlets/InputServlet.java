fb30b6faa5d4ef1bce306c24b1e4fb9163ca6c52
mara <marapislar@google.com>
2020-08-17 13:28:19 +0000

    Restructure code

--- src/main/java/com/google/sps/servlets/InputServlet.java
+++ src/main/java/com/google/sps/servlets/InputServlet.java
@@ -16,14 +16,8 @@ package com.google.sps.servlets;
  
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.cloud.language.v1.ClassificationCategory;
-import com.google.cloud.language.v1.ClassifyTextRequest;
-import com.google.cloud.language.v1.ClassifyTextResponse;
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.Document.Type;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
 import com.google.gson.Gson;
+import com.google.sps.data.TextAnalyser;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,110 +30,16 @@ import javax.servlet.http.HttpServletResponse;
  
 @WebServlet("/text-input")
 public class InputServlet extends HttpServlet {
- 
-  Map<String, String> requests;
- 
-  public void init() {
-     requests = new HashMap<String, String>();
-  }
- 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
- 
-    UserService userService = UserServiceFactory.getUserService();
-    String email = userService.getCurrentUser().getEmail();
-    
-    String input = requests.get(email);
- 
-    float score = getSentimentScore(input);
-    String mood = getMood(score);
-    ClassifyTextResponse analysed = analyseText(input);
- 
-    // list of all the categories that text is about
-    // check https://cloud.google.com/natural-language/docs/categories
-    ArrayList<String> categories = getCategories(analysed); //Classifying Content
- 
-    Gson gson = new Gson();
- 
-    response.setContentType("application/json;");
-    response.getWriter().println(gson.toJson(mood));
-  }
- 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    UserService userService = UserServiceFactory.getUserService();
- 
-    // Only logged-in users can post messages
-    if (!userService.isUserLoggedIn()) {
-      response.sendRedirect("/index.html");
-      return;
-    }
- 
     String input = request.getParameter("message");
-    String email = userService.getCurrentUser().getEmail();
+
+    TextAnalyser textAnalyser = new TextAnalyser(input);
+    Gson gson = new Gson();
  
-    requests.put(email, input);
- 
-    response.sendRedirect("/postcard.html");
-  }
- 
-  public String getMood(float score) {
-    if(score == 1) {
-      return "super super happy";
+    response.setContentType("text/html;");
+    for(String keyWord : textAnalyser.getKeyWords()) {
+      response.getWriter().println("<p>" + keyWord + "</p>");
     }
- 
-    if(score == -1) {
-        return "so pessimistic";
-    }
- 
-    int position = 0;
-    String[] moods = new String[]{"neutral", "calm", "relaxed", "serene", "contented",
-                                "joyful", "happy", "elated", "excited", "alert",
-                                "tense", "nervous", "stressed", "upset", "sad", 
-                                "depressed", "bored", "fatigued", "pessimisctic"};
- 
-    score = score * 10;
-    position = (int) score;
- 
-    if(position < 0) {
-        position = position * (-1) + 9;
-    }
- 
-    return moods[position];
-  }
- 
-  public static ClassifyTextResponse analyseText(String text) throws IOException {
-    try (LanguageServiceClient language = LanguageServiceClient.create()) {
-      Document document = 
-          Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
- 
-      ClassifyTextRequest request = ClassifyTextRequest.newBuilder().setDocument(document).build();
-      ClassifyTextResponse response = language.classifyText(request);
- 
-      return response;
-    }
-  }
- 
-  public static float getSentimentScore(String message) throws IOException {
-    try (LanguageServiceClient languageService = LanguageServiceClient.create()) {
-      Document doc =
-          Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
- 
-      Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
-      float score = sentiment.getScore();
- 
-      return score;
-    }
-  }
- 
-  public static ArrayList<String> getCategories(ClassifyTextResponse response) {
-    ArrayList<String> categories = new ArrayList<String>();
-    for (ClassificationCategory category : response.getCategoriesList()) {
-      String[] listCategories = category.getName().split("/");
-      for (int i = 0; i < listCategories.length; i++) {
-        categories.add(listCategories[i]);
-      }
-    }
-    return categories;
   }
 }
