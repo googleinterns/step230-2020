579f4e2118082a1ad12c438d7f66762f3ae4d69f
Krechals <topala.andrei@gmail.com>
2020-08-24 17:37:02 +0000

    modified code structure

--- src/main/webapp/script/postcardgen.js
+++ src/main/webapp/script/postcardgen.js
@@ -1,44 +1,65 @@
-function addImage(imgLink) {
-  const imgElem = document.createElement('img');
-  imgElem.className = 'pcard-img';
-  imgElem.setAttribute('src', imgLink);
-  
-  return imgElem;
+class Postcard {
+  constructor(title = '', message = '', imgUrl = '') {
+    this._title = title;
+    this._message = message;
+    this._img = imgUrl;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get message() {
+    return this._message;
+  }
+
+  get imgUrl() {
+    return this._imgUrl
+  }
+
+  addImageElem() {
+    const imgElem = document.createElement('img');
+    imgElem.className = 'pcard-img';
+    imgElem.setAttribute('src', this._img);
+    
+    return imgElem;
+  }
+
+  addMessageElem() {
+    const msgElem = document.createElement('div');
+    msgElem.className = 'pcard-msg';
+    msgElem.appendChild(document.createTextNode(this._message));
+
+    return msgElem;
+  }
+
+  addTitleElem() {
+    const titleElem = document.createElement('div');
+    titleElem.className = 'pcard-title';
+    titleElem.appendChild(document.createTextNode(this._title));
+
+    return titleElem;
+  }
+
+  getPostcard() {
+    const pcardElem = document.createElement('div');
+    pcardElem.className = 'pcard-container';
+    pcardElem.id = 'pcard-design';
+
+    pcardElem.appendChild(this.addTitleElem());
+    pcardElem.appendChild(this.addMessageElem());
+    pcardElem.appendChild(this.addImageElem());
+    return pcardElem;
+  }
 }
 
-function addMessage(messageTxt) {
-  const msgElem = document.createElement('div');
-  msgElem.className = 'pcard-msg';
-  msgElem.appendChild(document.createTextNode(messageTxt));
-
-  return msgElem;
-}
-
-function addTitle(titleTxt) {
-  const titleElem = document.createElement('div');
-  titleElem.className = 'pcard-title';
-  titleElem.appendChild(document.createTextNode(titleTxt));
-
-  return titleElem;
-}
-
-function addPcardElement(titleTxt, msgTxt, imgLink) {
-  const pcardElem = document.createElement('div');
-  pcardElem.className = 'pcard-container';
-  pcardElem.id = 'pcard-design';
-
-  pcardElem.appendChild(addTitle(titleTxt));
-  pcardElem.appendChild(addMessage(msgTxt));
-  pcardElem.appendChild(addImage(imgLink));
-  return pcardElem;
-}
-
-function getPostcard() {
+function displayPostcard() {
   const bodyElem = document.getElementsByTagName('body')[0];
   const title = "Happy birthday";
   const message = "Another adventure filled year awaits you.";
   const image = "https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&h=187&c=7&o=5&pid=1.7";
-  bodyElem.appendChild(addPcardElement(title, message, image));
+
+  bodyElem.appendChild(new Postcard(title, message, image).getPostcard());
 }
 
 function genPostcard() {
