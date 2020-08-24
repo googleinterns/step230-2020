cde9ff8232869da043621e30518224a2271109f1
Krechals <topala.andrei@gmail.com>
2020-08-24 13:16:12 +0000

    postcard injected on page

--- src/main/webapp/script/postcardgen.js
+++ src/main/webapp/script/postcardgen.js
@@ -1,3 +1,46 @@
+function addImage(imgLink) {
+  const imgElem = document.createElement('img');
+  imgElem.className = 'pcard-img';
+  imgElem.setAttribute('src', imgLink);
+  
+  return imgElem;
+}
+
+function addMessage(messageTxt) {
+  const msgElem = document.createElement('div');
+  msgElem.className = 'pcard-msg';
+  msgElem.appendChild(document.createTextNode(messageTxt));
+
+  return msgElem;
+}
+
+function addTitle(titleTxt) {
+  const titleElem = document.createElement('div');
+  titleElem.className = 'pcard-title';
+  titleElem.appendChild(document.createTextNode(titleTxt));
+
+  return titleElem;
+}
+
+function addPcardElement(titleTxt, msgTxt, imgLink) {
+  const pcardElem = document.createElement('div');
+  pcardElem.className = 'pcard-container';
+  pcardElem.id = 'pcard-design';
+
+  pcardElem.appendChild(addTitle(titleTxt));
+  pcardElem.appendChild(addMessage(msgTxt));
+  pcardElem.appendChild(addImage(imgLink));
+  return pcardElem;
+}
+
+function getPostcard() {
+  const bodyElem = document.getElementsByTagName('body')[0];
+  const title = "Happy birthday";
+  const message = "Another adventure filled year awaits you.";
+  const image = "https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&h=187&c=7&o=5&pid=1.7";
+  bodyElem.appendChild(addPcardElement(title, message, image));
+}
+
 function genPostcard() {
   const node = document.getElementById('pcard-design');
 
