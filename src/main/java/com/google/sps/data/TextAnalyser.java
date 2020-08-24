58a4ad93946c3c9f24e6ad293aca57c968b1e2a9
mara <marapislar@google.com>
2020-08-24 07:18:15 +0000

    Add function to make the api testing possible.

--- src/main/java/com/google/sps/data/TextAnalyser.java
+++ src/main/java/com/google/sps/data/TextAnalyser.java
@@ -10,6 +10,9 @@
 
 package com.google.sps.data;
 
+
+import com.google.api.gax.rpc.HeaderProvider;
+import com.google.api.gax.rpc.FixedHeaderProvider;
 import com.google.cloud.language.v1.AnalyzeEntitiesRequest;
 import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
 import com.google.cloud.language.v1.ClassificationCategory;
@@ -21,6 +24,7 @@ import com.google.cloud.language.v1.EncodingType;
 import com.google.cloud.language.v1.Entity;
 import com.google.cloud.language.v1.EntityMention;
 import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.LanguageServiceSettings;
 import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -30,17 +34,30 @@ import java.util.List;
 import java.util.Set;
 
 /*
-* This class creates objects that analyse text and 
+* This class creates objects that analyse text by 
+* extracting the categories, the mood, the events.
+* The scope is to get a final set of the key words
+* from the text. 
 */
+
 public final class TextAnalyser {
   private final String message;
+  private static final double EPSILON = 0.00001;
 
   public TextAnalyser(String message) {
      this.message = message.toLowerCase();
   }
 
+  public final LanguageServiceSettings getSettings() throws IOException {
+    HeaderProvider headerProvider =
+            FixedHeaderProvider.create("X-Goog-User-Project","google.com:gpostcard");
+    return LanguageServiceSettings.newBuilder()
+                .setHeaderProvider(headerProvider)
+                .build();
+  }
+
   public float getSentimentScore() throws IOException {
-    try (LanguageServiceClient languageService = LanguageServiceClient.create()) {
+    try (LanguageServiceClient languageService = LanguageServiceClient.create(getSettings())) {
       Document doc =
         Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
       Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
@@ -51,12 +68,12 @@ public final class TextAnalyser {
 
   public String getMood() throws IOException {
     float score = getSentimentScore();
-    
-    if(score == 1) {
+
+    if (Math.abs(score - 1) < EPSILON) {
       return "very happy";
     }
  
-    if(score == -1) {
+    if (Math.abs(score + 1) < EPSILON) {
       return "so pessimistic";
     }
  
@@ -69,40 +86,43 @@ public final class TextAnalyser {
     score = score * 10;
     position = (int) score;
  
-    if(position < 0) {
+    if (position < 0) {
       position = position * (-1) + 9;
     }
  
     return moods[position];
   }
 
-  private ClassifyTextResponse classify() {
-    try (LanguageServiceClient language = LanguageServiceClient.create()) {
+  private ClassifyTextResponse classify() throws IOException {
+    try (LanguageServiceClient language = LanguageServiceClient.create(getSettings())) {
       Document document = 
             Document.newBuilder().setContent(message).setType(Type.PLAIN_TEXT).build();
       ClassifyTextRequest request = ClassifyTextRequest.newBuilder().setDocument(document).build();
-        
+      
       return language.classifyText(request);
-    } catch(Exception e) {
+    } catch (Exception e) {
+      System.err.println("Not enough tokens (words) to actually get a category.");
       return null;
     }
   }
 
   // list of all the categories that text is about
   // check https://cloud.google.com/natural-language/docs/categories
-  public List<String> getCategories() {
+  public Set<String> getCategories() throws IOException {
     ClassifyTextResponse response = classify();
 
-    if(response == null) {
-      return Collections.emptyList();
+    // no categories could be identified
+    if (response == null) {
+      return Collections.emptySet();
     }
       
-    ArrayList<String> categories = new ArrayList<String>();
+    Set<String> categories = new HashSet<String>();
 
     for (ClassificationCategory category : response.getCategoriesList()) {
       String[] listCategories = category.getName().split("/");
       for (int i = 0; i < listCategories.length; i++) {
         categories.add(listCategories[i].toLowerCase());
+        System.out.println(listCategories[i].toLowerCase());
       }
     }
 
@@ -116,11 +136,10 @@ public final class TextAnalyser {
                                     "gratitude", "job", "promotion",
                                     "new", "welcome", "good evening", "good night",
                                     "holiday"};
-    String copy = new String(message);
-    copy = copy.toLowerCase();
+    String copy = message;
 
     for (int i = 0; i < allEvents.length; i++) {
-      if(copy.indexOf(allEvents[i]) != -1) {
+      if (copy.indexOf(allEvents[i]) != -1) {
         events.add(allEvents[i]);
       }
     }
@@ -130,22 +149,20 @@ public final class TextAnalyser {
 
   /** Identifies entities in the string */
   private AnalyzeEntitiesResponse analyzeEntitiesText() throws IOException {
-    // Instantiate the Language client com.google.cloud.language.v1.LanguageServiceClient
-    try (LanguageServiceClient language = LanguageServiceClient.create()) {
+    try (LanguageServiceClient language = LanguageServiceClient.create(getSettings())) {
       Document doc = Document.newBuilder().setContent(message).setType(Type.PLAIN_TEXT).build();
       AnalyzeEntitiesRequest request =
           AnalyzeEntitiesRequest.newBuilder()
               .setDocument(doc)
               .setEncodingType(EncodingType.UTF16)
               .build();
-
       return language.analyzeEntities(request);
     }
   }
 
-  public List<String> getEntities() throws IOException {
+  public Set<String> getEntities() throws IOException {
     AnalyzeEntitiesResponse response = analyzeEntitiesText();
-    List<String> entities = new ArrayList<String>();
+    Set<String> entities = new HashSet<String>();
 
     for (Entity entity : response.getEntitiesList()) {
       entities.add(entity.getName());
@@ -156,14 +173,19 @@ public final class TextAnalyser {
 
   // put all the key words together
   // use a HashSet to remove duplicates
-  public Set<String> getKeyWords() throws IOException {
-    Set<String> keyWords = new HashSet<String>();
+  public Set<String> getKeyWords() {
+    try {
+      Set<String> keyWords = new HashSet<String>();
+      keyWords.addAll(getEvents());
+      keyWords.addAll(getEntities());
+      keyWords.addAll(getCategories());
+      keyWords.add(getMood());
 
-    keyWords.addAll(getEvents());
-    keyWords.addAll(getEntities());
-    keyWords.addAll(getCategories());
-    keyWords.add(getMood());
-
-    return keyWords;
+      return keyWords;
+    } catch (IOException e) {
+      // no key words  
+      System.err.println("There are no key words.");
+      return Collections.emptySet();
+    }
   }
 }
