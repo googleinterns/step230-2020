d28c50914900942c5213d85e3da378da42c8f6cb
mara <marapislar@google.com>
2020-08-24 13:00:47 +0000

    Change names of the tests and fix review issues.

--- src/test/java/com/google/sps/TextAnalyserTest.java
+++ src/test/java/com/google/sps/TextAnalyserTest.java
@@ -2,59 +2,59 @@ package com.google.sps.data;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.junit.Test;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+
+/**
+* This class tests the functions from the TextAnalyser class.
+*
+* The format of a test name is:
+* 1) conditions
+* 2) what is the subject being tested (the input)
+* 3) expected results (the output)
+* These concepts are separated by _ e.g. conditions_input_output
+**/
 
 @RunWith(JUnit4.class)
 public final class TextAnalyserTest {
 
   @Test
-  public void analysePositiveSentiment() {
-    try {
-      TextAnalyser textAnalyser = new TextAnalyser("Today is a special day. It is your birthday, so enjoy it!");
-      assertTrue("Sentiment score is not positive.", textAnalyser.getSentimentScore() > 0.0F);
-    } catch (IOException e) {
-      fail(e.getMessage());
-    }
+  public void analyse_positiveText_positiveSentimentScore() throws IOException {
+    TextAnalyser textAnalyser = new TextAnalyser("Today is a special day. It is your birthday, so enjoy it!");
+    assertTrue("Sentiment score is not positive.", textAnalyser.getSentimentScore() > 0.0F);
   }
 
   @Test
-  public void analyseNegativeSentiment() {
-    try {
-      TextAnalyser textAnalyser = new TextAnalyser("I don't like this.");
-      assertTrue("Sentiment score is not negative.", textAnalyser.getSentimentScore() < 0.0F);
-    } catch (IOException e) {
-      fail(e.getMessage());
-    }
+  public void analyse_negativeText_negativeSentimentScore() throws IOException {
+    TextAnalyser textAnalyser = new TextAnalyser("I don't like this.");
+    assertTrue("Sentiment score is not negative.", textAnalyser.getSentimentScore() < 0.0F);
   }
 
   @Test
-  public void analysePositiveMood() throws IOException {
+  public void analyse_positiveText_positiveMood() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("Happy Birthday!");
-    String expected = "thrilled";
-    String actual = textAnalyser.getMood();
 
-    assertEquals(expected, actual);
+    assertEquals("thrilled", textAnalyser.getMood());
   }
 
   @Test
-  public void analyseNegativeMood() throws IOException {
+  public void analyse_negativeText_negativeMood() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("Smoking, well, there is no healthy amount of smoking." +
                                                   "Healthy here meant never having smoked.");
-    String expected = "depressed";
-    String actual = textAnalyser.getMood();
 
-    assertEquals(expected, actual);
+    assertEquals("depressed", textAnalyser.getMood());
   }
 
   @Test
-  public void testForCorrectEvents() {
+  public void analyse_textWithEvents_eventsPresent() {
     TextAnalyser textAnalyser = new TextAnalyser("Today is my Birthday. Today you got a job promotion.");
     Set<String> expected = new HashSet<>();
     Set<String> actual = textAnalyser.getEvents();
@@ -67,25 +67,25 @@ public final class TextAnalyserTest {
   }
 
   @Test
-  public void testEntityThatReturnsLocation() throws IOException {
+  public void analyse_textWithLocation_locationPresent() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("I am going to Paris this weekend.");
     assertTrue("There is no city displayed.", textAnalyser.getEntities().contains("paris"));
   }
 
   @Test
-  public void testEntityThatReturnsName() throws IOException {
+  public void analyse_textWithName_namePresent() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("Hello, Mark!");
     assertTrue("There is no name displayed", textAnalyser.getEntities().contains("mark"));
   }
 
   @Test
-  public void testEntityThatReturnsEvent() throws IOException {
+  public void analyse_textWithEventEntity_eventPresent() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("My wedding is coming soon!");
     assertTrue("There is no event displayed.", textAnalyser.getEntities().contains("wedding"));
   }
 
   @Test
-  public void testOnlyLowerCaseKeyWords() throws IOException {
+  public void toLowerCase_KeyWords_lowerCaseKeyWords() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("Hello, Mark! My Birthday is coming very soon and I want to" +
                                                   " invite you to my Party! It is in Paris. It is not expensive." +
                                                   " We will have so much fun! All of our friends will also come to the party");
@@ -100,20 +100,13 @@ public final class TextAnalyserTest {
   }
 
   @Test
-  public void testCorrectKeyWords() throws IOException {
+  public void analyse_textContainingKeyWords_notEmptySet() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("Happy Birthday! We will go to Paris!");
-    Set<String> expected = new HashSet<>();
-    Set<String> actual = textAnalyser.getKeyWords();
-
-    expected.add("birthday");
-    expected.add("paris");
-    expected.add("serene");
-
-    assertEquals(expected, actual);
+    assertFalse(textAnalyser.getKeyWords().isEmpty());
   }
 
   @Test
-  public void testEnoughWordsToGetCategory() throws IOException {
+  public void analyse_textWithEnoughWordsToGetCategory_theCategory() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("A computer is a machine that can be instructed to carry" +
                                                  " out sequences of arithmetic or logical operations automatically" +
                                                  " via computer programming. Modern computers have the ability to" +
@@ -122,11 +115,9 @@ public final class TextAnalyserTest {
   }
 
   @Test
-  public void testNotEnoughWordsToGetCategory() throws IOException {
+  public void analyse_textWithNotEnoughWordsToGetCateogry_emptySet() throws IOException {
     TextAnalyser textAnalyser = new TextAnalyser("Hello!");
-    Set<String> expected = new HashSet<>();
-    Set<String> actual = textAnalyser.getCategories();
 
-    assertEquals(expected, actual);
+    assertEquals(Collections.emptySet(), textAnalyser.getCategories());
   }
 }
