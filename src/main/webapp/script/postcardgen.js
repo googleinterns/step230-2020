b98de6c409b10755bbcea66cd26afc9da168de7a
Krechals <topala.andrei@gmail.com>
2020-08-25 18:51:26 +0000

    more tests added

--- src/main/webapp/script/postcardgen.js
+++ src/main/webapp/script/postcardgen.js
@@ -41,7 +41,7 @@ class Postcard {
     return titleElem;
   }
 
-  getPostcard() {
+  getPostcardHTML() {
     const pcardElem = document.createElement('div');
     pcardElem.className = 'pcard-container';
     pcardElem.id = 'pcard-design';
@@ -51,25 +51,28 @@ class Postcard {
     pcardElem.appendChild(this.addImageElem());
     return pcardElem;
   }
+
+  // The postcard needs to be already on the page.
+  // Getting the postcard directly from getPostcardHTML() does not work.
+  getPostcardImage() {
+    let node = document.getElementById('pcard-design');
+    let img = document.createElement('img');
+    domtoimage.toPng(node).then(function (dataUrl) {
+      img.setAttribute('src', dataUrl);
+    }).catch(function (error) {
+      console.error('oops, something went wrong!', error);
+      img.setAttribute('src', '');
+    });
+    return img;
+  }
 }
 
 function displayPostcard() {
   const bodyElem = document.getElementsByTagName('body')[0];
   const title = "Happy birthday";
   const message = "Another adventure filled year awaits you.";
-  const image = "https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&h=187&c=7&o=5&pid=1.7";
+  const imgUrl = "https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&h=187&c=7&o=5&pid=1.7";
 
-  bodyElem.appendChild(new Postcard({title, message, image}).getPostcard());
-}
-
-function genPostcard() {
-  const node = document.getElementById('pcard-design');
-
-  domtoimage.toPng(node).then(function (dataUrl) {
-    const img = new Image();
-    img.src = dataUrl;
-    document.body.appendChild(img);
-  }).catch(function (error) {
-    console.error('oops, something went wrong!', error);
-  });
+  bodyElem.appendChild(new Postcard({title, message, imgUrl}).getPostcardHTML());
+  document.body.appendChild(new Postcard({title, message, imgUrl}).getPostcardImage());
 }
--- src/test/webapp/script/postcardgen_test.js
+++ src/test/webapp/script/postcardgen_test.js
@@ -1,5 +1,5 @@
 
-describe('postcard', () => {
+describe('Postcard - check constructor', () => {
   it('should have title', () => {
     const title = 'Happy birthday!';
     let postcard = new Postcard({title});
@@ -16,5 +16,39 @@ describe('postcard', () => {
     const imgUrl = 'https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&h=187&c=7&o=5&pid=1.7';
     let postcard = new Postcard({imgUrl});
     expect(postcard.img).toBe(imgUrl);
+  });
+});
+
+describe('Postcard - generate elements', () => {
+  it('should generate title element', () => {
+    const title = 'Happy birthday!';
+    let postcard = new Postcard({title});
+    expect(postcard.addTitleElem().outerHTML).toBe('<div class="pcard-title">' + title + '</div>');
+  });
+
+  it('should generate message element', () => {
+    const message = 'Another adventure filled year awaits you.';
+    let postcard = new Postcard({message});
+    expect(postcard.addMessageElem().outerHTML).toBe('<div class="pcard-msg">' + message + '</div>');
+  });
+
+  it('should generate image element', () => {
+    const imgUrl = 'https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&h=187&c=7&o=5&pid=1.7';
+    let postcard = new Postcard({imgUrl});
+    expect(postcard.addImageElem().outerHTML).toMatch('^<img class="pcard-img" src="https:\/\/tse2.mm.bing.net\/th\/id\/.*>$');
+  });
+});
+
+describe('Postcard - add elements togehter', () => {
+  it('should generate postcard HTML element', () => {
+    const title = 'Happy birthday!';
+    const message = 'Another adventure filled year awaits you.';
+    const imgUrl = 'https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&h=187&c=7&o=5&pid=1.7';
+    let postcard = new Postcard({title, message, imgUrl});
+    expect(postcard.getPostcardHTML().outerHTML).toBe('<div class="pcard-container" id="pcard-design">' +
+                '<div class="pcard-title">' + title + '</div>' +
+                '<div class="pcard-msg">' + message + '</div>' +
+                '<img class="pcard-img" src="https://tse2.mm.bing.net/th/id/OIP.geCKcqpyVwMD6EJuAT3lVQHaEK?w=333&amp;h=187&amp;c=7&amp;o=5&amp;pid=1.7">' +
+                '</div>');
   });
 });
