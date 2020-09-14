92f3fe510bca124492a211b3e51bc2996fc2da60
Katya <solzhenitsyna@google.com>
2020-09-14 12:15:53 +0000

    sync with branch

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -130,6 +130,34 @@ function geocodeLatLng(position) {
   );
 }
 
+function record() {
+  const recognition = new webkitSpeechRecognition();
+  const message = document.getElementById("recording-message");
+  recognition.lang = "en-GB";
+
+  recognition.onresult = function(event) {
+    document.getElementById("input_text").value = event.results[0][0].transcript;
+  }
+
+  recognition.addEventListener('nomatch', function() { 
+    message.innerText = "SPEECH NOT RECOGNISED";
+  });
+
+  recognition.onaudiostart = function() {
+    message.innerText = "RECORDING...";
+  }
+
+  recognition.onaudioend = function() {
+    message.innerText = "DONE RECORDING";
+  }
+
+  recognition.onerror = function() {
+    message.innerText = "SPEECH NOT RECOGNISED";
+  }
+
+  recognition.start();
+}
+
 
  
 
